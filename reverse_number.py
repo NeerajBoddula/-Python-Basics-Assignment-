697b48aad15913b42c710ed9f683cf6a18befa67
Neeraj Boddula <neerajboddula75@gmail.com>
2025-07-02 21:22:18 +0530

    Create reverse_number.py

---
+++ reverse_number.py
@@ -0,0 +1,11 @@
+def reverse_number(n):
+    reversed_num = 0
+    while n > 0:
+        digit = n % 10
+        reversed_num = reversed_num * 10 + digit
+        n = n // 10
+    return reversed_num
+
+# Example usage
+num = 1234
+print("Reversed:", reverse_number(num))
